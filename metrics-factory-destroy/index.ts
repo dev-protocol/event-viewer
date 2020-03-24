b72e08c92089d7d95f47a48b33d1e1365265f079
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-03-24 10:28:50 +0900

    add metrics factory destroy event

---
+++ metrics-factory-destroy/index.ts
@@ -0,0 +1,44 @@
+import { AzureFunction, Context } from '@azure/functions'
+import { ObjectType } from 'typeorm'
+import { EventSaver } from '../common/base'
+import { MetricsFactoryDestroy } from '../entities/metrics-factory-destroy'
+import abi from './abi.json'
+
+class DestroyEventSaver extends EventSaver {
+	getModelObject<Entity>(): ObjectType<Entity> {
+		return MetricsFactoryDestroy
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-untyped-public-signature
+	getSaveData(event: Map<string, any>): any {
+		const metricsFactoryCreate = new MetricsFactoryDestroy()
+		const values = event.get('returnValues')
+		// eslint-disable-next-line @typescript-eslint/camelcase
+		metricsFactoryCreate.from_address = values._from
+		metricsFactoryCreate.metrics = values._metrics
+		return metricsFactoryCreate
+	}
+
+	getBatchName(): string {
+		return 'metrics-factory-destroy'
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-untyped-public-signature
+	getAbi(): any {
+		return abi
+	}
+
+	getEventName(): string {
+		return 'Destroy'
+	}
+}
+
+const timerTrigger: AzureFunction = async function(
+	context: Context,
+	myTimer: any
+): Promise<void> {
+	const saver = new DestroyEventSaver(context, myTimer)
+	await saver.execute()
+}
+
+export default timerTrigger
