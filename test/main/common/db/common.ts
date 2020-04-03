75914907275920355cbaf41bda94fe0d46b32ff6
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-04-03 11:26:46 +0900

    se environment

--- test/main/common/db/common.ts
+++ test/main/common/db/common.ts
@@ -2,8 +2,14 @@ import { DbConnection } from '../../../../common/db/common'
 
 describe('DbConnection', () => {
 	it('connect.', async () => {
+		process.env.DB_HOST = 'localhost'
+		process.env.DB_PORT = '5432'
+		process.env.DB_USERNAME = 'testuser'
+		process.env.DB_PASSWORD = 'testpassword'
+		process.env.DB_DATABASE = 'testdb'
 		const con = new DbConnection('test-batch-name')
 		await con.connect()
+		console.log(111)
 		expect(con.connection.isConnected).toBe(true)
 	})
 })
