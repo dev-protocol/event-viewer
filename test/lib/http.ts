7bd0b177263a5d33453a81d010ab8cda6793991a
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-04-06 17:21:02 +0900

    add validator test

---
+++ test/lib/http.ts
@@ -0,0 +1,19 @@
+import { HttpRequest } from '@azure/functions'
+
+export function getHttpRequestMockBase(
+	headersArg: {
+		[key: string]: string
+	},
+	bodyArg: any
+): HttpRequest {
+	const mockHttpReq: HttpRequest = {
+		method: 'POST',
+		url: 'https://dummy-domein',
+		headers: headersArg,
+		query: {},
+		params: {},
+		body: bodyArg,
+		rawBody: undefined
+	}
+	return mockHttpReq
+}
