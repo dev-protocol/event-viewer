80472d1422284a1bd508c804d786a7f94583c8a2
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-04-07 11:18:11 +0900

    delete comment out

--- test/main/common/db/common.ts
+++ test/main/common/db/common.ts
@@ -1,32 +1,32 @@
-// Import { LockupLockedup } from '../../../../entities/lockup-lockedup'
-// import { getDbConnection } from './../../../lib/db'
-// import { saveLockupLockupedTestdata } from './../../../lib/test-data'
+import { LockupLockedup } from '../../../../entities/lockup-lockedup'
+import { getDbConnection } from './../../../lib/db'
+import { saveLockupLockupedTestdata } from './../../../lib/test-data'
 
 describe('DbConnection', () => {
 	it('can connect to db.', async () => {
-		// Const con = await getDbConnection()
-		// expect(con.connection.isConnected).toBe(true)
-		// await con.quit()
+		const con = await getDbConnection()
+		expect(con.connection.isConnected).toBe(true)
+		await con.quit()
 	})
 })
 
 describe('Transaction', () => {
 	it('can save data when you commit.', async () => {
-		// Const con = await getDbConnection()
-		// await saveLockupLockupedTestdata(con.connection, true)
-		// const recordCount = await con.connection
-		// 	.getRepository(LockupLockedup)
-		// 	.count()
-		// await con.quit()
-		// expect(recordCount).toBe(3)
+		const con = await getDbConnection()
+		await saveLockupLockupedTestdata(con.connection, true)
+		const recordCount = await con.connection
+			.getRepository(LockupLockedup)
+			.count()
+		await con.quit()
+		expect(recordCount).toBe(3)
 	})
 	it('cannot save data when you rollback.', async () => {
-		// Const con = await getDbConnection()
-		// await saveLockupLockupedTestdata(con.connection, false)
-		// const recordCount = await con.connection
-		// 	.getRepository(LockupLockedup)
-		// 	.count()
-		// await con.quit()
-		// expect(recordCount).toBe(0)
+		const con = await getDbConnection()
+		await saveLockupLockupedTestdata(con.connection, false)
+		const recordCount = await con.connection
+			.getRepository(LockupLockedup)
+			.count()
+		await con.quit()
+		expect(recordCount).toBe(0)
 	})
 })
