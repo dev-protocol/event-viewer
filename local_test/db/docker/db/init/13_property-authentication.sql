55da51b34c257fe76337da3f8f67444da1255297
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-04-20 14:39:35 +0900

    change primary key

--- local_test/db/docker/db/init/13_property-authentication.sql
+++ local_test/db/docker/db/init/13_property-authentication.sql
@@ -1,13 +1,12 @@
 DROP TABLE IF EXISTS property_authentication;
 
 CREATE TABLE property_authentication(
-	id BIGSERIAL NOT NULL,
-    block_number INT NOT NULL,
     property TEXT NOT NULL,
     metrics TEXT NOT NULL,
+    block_number INT NOT NULL,
     market TEXT NOT NULL,
     authentication_id TEXT NOT NULL,
-	PRIMARY KEY(id)
+    PRIMARY KEY(property, metrics)
 );
 
 
